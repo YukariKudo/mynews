cadafaca26541507a2cdd34e25422b36bc38f4d1
名前 <>
2021-07-20 11:47:04 +0000

    PHP-Laravel-16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,16 +4,46 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\profile;
 
 class ProfileController extends Controller
 {
     //
-    public function add(){
+    public function add()
+    {
         return view('admin.profile.create');
     }
     public function create(Request $request){
-        return redirect('admin/profile/create');
-    }
+    $this->validate($request, Profile::$rules);
+      $profiles = new Profile;
+      $form = $request->all();
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profiles->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      
+      unset($form['_token']);
+      unset($form['image']);
+      $profile->fill($form);
+      $profile->save();
+       
+       return redirect('admin/profile/create');}
+       
+       public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+   
     public function edit(){
         return view('admin.profile.edit');
     }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
  Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function(){
   Route::get('news/create', 'Admin\NewsController@add');
   Route::post('news/create', 'Admin\NewsController@create');
-  Route::post('profile/create', 'Admin\NewsController@create');
+  Route::post('profile/create', 'Admin\ProfileController@create');
   Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
@@ -26,6 +26,11 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth'); 
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+    Route::get('profile', 'Admin\ProfileController@index')->middleware('auth');
+    
 });
 
 Auth::routes();
