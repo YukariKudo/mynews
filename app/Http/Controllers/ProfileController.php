a89f8b3c25bcfb1ad82778c3a4c93a73408c8cc9
名前 <>
2021-07-22 08:28:26 +0000

    PHP-laravel-deploy

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,9 +10,6 @@ class ProfileController extends Controller
     public function index(Request $request)
     {
         $posts = Profile::all()->sortByDesc('updated_at');
-
-        
-
         // news/index.blade.php ファイルを渡している
         // また View テンプレートに headline、 posts、という変数を渡している
         return view('profile.index', ['posts' => $posts]);
